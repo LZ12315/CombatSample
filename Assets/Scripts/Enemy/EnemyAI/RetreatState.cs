ea3b9b3e6d030f93d78e9586ad6a47d442d6c85c
LZ12315 <yang2005222021@126.com>
2025-05-31 16:56:59 +0800

    Promote AI Attack

---
+++ Assets/Scripts/Enemy/EnemyAI/RetreatState.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RetreatState : State<EnemyController>
+{
+    [SerializeField] private float backwardSpeed = 0.75f;
+    [SerializeField] private float distanceToRetreat = 3f;
+    Vector3 lastPosition = Vector3.zero;
+
+    public override void OnEnter(EnemyController owner)
+    {
+        base.OnEnter(owner);
+        _owner.NavAgent.ResetPath();
+    }
+
+    public override void OnUpdate()
+    {
+        float distanceToTarget = Vector3.Distance(_owner.transform.position, _owner.Target.position);
+        if(distanceToTarget >= distanceToRetreat)
+        {
+            _owner.ChangeState(Utils.Enums.EnemyStates.CombatMove);
+            return;
+        }
+
+        var vectorToTarget = _owner.Target.position - _owner.transform.position;
+
+        _owner.NavAgent.Move(- vectorToTarget.normalized * backwardSpeed * Time.deltaTime);
+
+        Vector3 velocity = ((_owner.NavAgent.nextPosition - lastPosition) / Time.deltaTime);
+        _owner.LocalMotion(vectorToTarget, velocity.normalized, backwardSpeed);
+
+        _owner.NavAgent.nextPosition = _owner.transform.position;
+        lastPosition = _owner.NavAgent.nextPosition;
+    }
+
+    public override void OnExit()
+    {
+        base.OnExit();
+        _owner.NavAgent.ResetPath();
+        _owner.LocalMotion(Vector3.zero);
+    }
+
+}
