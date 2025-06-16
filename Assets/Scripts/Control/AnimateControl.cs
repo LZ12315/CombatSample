d3a9d198e599ef8a2aec0b59f374e235e543f47a
LZ12315 <yang2005222021@126.com>
2025-06-17 01:41:27 +0800

    GuardPromote

--- Assets/Scripts/Control/AnimateControl.cs
+++ Assets/Scripts/Control/AnimateControl.cs
@@ -6,12 +6,14 @@ public class AnimateControl : MonoBehaviour
 {
     private Animator animator;
 
-    [field:Header("注视设置")]
-    [SerializeField] private Transform head = null;
+    [field: Header("注视设置")]
+    [field: SerializeField] public Transform Head { get; private set; }
     [SerializeField] private float lookAtCoolTime = 0.2f;
     [SerializeField] private float lookAtHeatTime = 0.2f;
-    public Vector3 lookAtTargetPosition { get; set;}
-    public bool looking { get; set; } = true;
+
+    public Vector3 LookAtTargetPosition { get; set; }
+    public bool Looking { get; set; } = true;
+    public Vector3 OriginLookDirection { get; set; }
 
     private Vector3 lookAtPosition;
     private float lookAtWeight = 0.0f;
@@ -21,13 +23,14 @@ public class AnimateControl : MonoBehaviour
         if (animator == null)
             animator = GetComponent<Animator>();
 
-        if (!head)
+        if (!Head)
         {
-            looking = false;
+            Looking = false;
             return;
         }
-        lookAtTargetPosition = head.position + transform.forward;
-        lookAtPosition = lookAtTargetPosition;
+        OriginLookDirection = Head.position + transform.forward;
+        LookAtTargetPosition = OriginLookDirection;
+        lookAtPosition = LookAtTargetPosition;
     }
 
     //当该动画层启用IK Pass时 这个回调函数才会被触发
@@ -38,16 +41,16 @@ public class AnimateControl : MonoBehaviour
 
     void LookIK()
     {
-        Vector3 modifiedTargetPos = lookAtTargetPosition;
-        modifiedTargetPos.y = head.position.y;
-        lookAtTargetPosition = modifiedTargetPos;
-        float lookAtTargetWeight = looking ? 1.0f : 0.0f;
+        Vector3 modifiedTargetPos = LookAtTargetPosition;
+        modifiedTargetPos.y = Head.position.y;
+        LookAtTargetPosition = modifiedTargetPos;
+        float lookAtTargetWeight = Looking ? 1.0f : 0.0f;
 
-        Vector3 curDir = lookAtPosition - head.position;
-        Vector3 futDir = lookAtTargetPosition - head.position;
+        Vector3 curDir = lookAtPosition - Head.position;
+        Vector3 futDir = LookAtTargetPosition - Head.position;
 
         curDir = Vector3.RotateTowards(curDir, futDir, 6.28f * Time.deltaTime, float.PositiveInfinity);
-        lookAtPosition = head.position + curDir;
+        lookAtPosition = Head.position + curDir;
 
         float blendTime = lookAtTargetWeight > lookAtWeight ? lookAtHeatTime : lookAtCoolTime;
         lookAtWeight = Mathf.MoveTowards(lookAtWeight, lookAtTargetWeight, Time.deltaTime / blendTime);
