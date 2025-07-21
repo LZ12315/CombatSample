960ed4cd35d13a1832b5dddb39a99d3c956a56ee
LZ12315 <yang2005222021@126.com>
2025-07-21 15:54:07 +0800

    Nothing

---
+++ Assets/Scripts/Player/ActionDirector/ActionTransitionAsset.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ActionTransitionAsset : PlayableAsset
+{
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+    {
+        var playable = ScriptPlayable<ActionTransitionClip>.Create(graph);
+        ActionTransitionClip clip = playable.GetBehaviour();
+        return playable;
+    }
+
+}
+
+public class ActionTransitionClip : PlayableBehaviour
+{
+    PlayerController controller;
+    bool isPlaying = false;
+
+    public override void OnBehaviourPlay(Playable playable, FrameData info)
+    {
+        if(isPlaying) return;
+        isPlaying = true;
+    }
+
+    public override void OnBehaviourPause(Playable playable, FrameData info)
+    {
+        if(!isPlaying) return;
+        isPlaying = false;
+    }
+
+}
