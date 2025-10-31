25c880ee4bd347173920b105b58f7233243e3026
LZ12315 <yang2005222021@126.com>
2025-10-31 21:26:12 +0800

    Nothing

--- Assets/Scripts/TimelinePlayable/Effect/EffectTrack.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Timeline;
-
-[TrackColor(0.2f, 0.6f, 1.0f)]
-[TrackClipType(typeof(EffectAsset))]
-public class EffectTrack : ActionTrackBase { }
